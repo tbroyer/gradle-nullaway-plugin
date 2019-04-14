7268484b66747f923d965ff3c34317b1000d07b9
Thomas Broyer <t.broyer@ltgt.net>
2019-04-14 18:03:25 +0200

    Project extension does not need to be Named

--- src/main/kotlin/net/ltgt/gradle/nullaway/NullAwayExtension.kt
+++ src/main/kotlin/net/ltgt/gradle/nullaway/NullAwayExtension.kt
@@ -1,13 +1,10 @@
 package net.ltgt.gradle.nullaway
 
-import org.gradle.api.Named
 import org.gradle.api.model.ObjectFactory
 import org.gradle.kotlin.dsl.listProperty
 
 open class NullAwayExtension internal constructor(
     objectFactory: ObjectFactory
-) : Named {
-    override fun getName() = EXTENSION_NAME
-
+) {
     val annotatedPackages = objectFactory.listProperty<String>()
 }
