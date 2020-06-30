c853e62e6e596ccf3ce6b2b62eab735a16d3862d
Thomas Broyer <t.broyer@ltgt.net>
2020-06-30 16:29:08 +0200

    Add Gradle GitHub Actions Plugin to tag build scans

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     id("com.gradle.plugin-publish") version "0.12.0"
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
     id("com.android.lint") version "3.6.3"
+    id("org.nosphere.gradle.github.actions") version "1.2.0"
 }
 buildscript {
     dependencyLocking {
