6e24d7a5b976ebab9365a5f0d1a9df9fe1f771b5
Thomas Broyer <t.broyer@ltgt.net>
2024-02-04 18:06:54 +0100

    Migrate to Develocity injection on CI

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,5 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-plugins {
-    id("com.gradle.enterprise") version "3.11.3"
-}
-gradleEnterprise {
-    buildScan {
-        termsOfServiceUrl = "https://gradle.com/terms-of-service"
-        termsOfServiceAgree = "yes"
-    }
-}
 
 rootProject.name = "gradle-nullaway-plugin"
